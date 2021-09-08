5acba4cc518cc151cc06931a6fb9c72ea6dca33f
Rob Granciosa <rgranciosa@gmail.com>
2021-09-08 08:33:20 -0400

    Add search module

--- the-outer-edge-frontend/src/app/app.module.ts
+++ the-outer-edge-frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FormComponent } from './form/form.component';
 import { FormTableComponent } from './form-table/form-table.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
     FormComponent,
-    FormTableComponent
+    FormTableComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
